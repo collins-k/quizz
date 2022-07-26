a0709de33af7a901a43a375f5601d3bab51ce3fe
collins-k <cyrille_08kamgaing@yahoo.fr>
2022-07-26 17:16:58 +0200

    feat(): import lottie player

---
+++ frontend/src/utils/index.tsx
@@ -0,0 +1,7 @@
+import loader from "./loader.json";
+import up from "./up.json";
+import confetti from "./confetti.json";
+import identity from "./identity.json";
+import wallet from "./wallet.json";
+
+export{loader, up, confetti, identity, wallet};
