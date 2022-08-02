bbdb6e3b3646022d8cb64fe1fa212971caafe2f6
collins-k <cyrille_08kamgaing@yahoo.fr>
2022-08-03 00:27:36 +0200

    feat(): update comments

--- frontend/src/components/Dapp.tsx
+++ frontend/src/components/Dapp.tsx
@@ -22,10 +22,9 @@ import {Loading} from "./Loading";
 import {QuizNotFound} from "./QuizNotFound";
 import {IQuiz, IState} from "../models/DappState";
 
-// This is the Hardhat Network id that we set in our hardhat.config.js.
-// Here's a list of network ids https://docs.metamask.io/guide/ethereum-provider.html#properties
-// to use when deploying to other networks.
+// This is the Hardhat Network id that we set in our hardhat.config.js for local development.
 const HARDHAT_NETWORK_ID = '1337';
+const POLYGON_NETWORK_ID = '80001';
 
 // This is an error code that indicates that the user canceled a transaction
 const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
@@ -45,7 +44,7 @@ declare let window: any;
 export class Dapp extends React.Component<{}, IState> {
     // We store multiple things in Dapp's state.
     initialState = {
-        // The current quiz's contract
+        // The current quiz's contract and its metadata
         qContract: this._getInitQuizState(),
         // The user's address
         selectedAddress: undefined,
@@ -85,13 +84,6 @@ export class Dapp extends React.Component<{}, IState> {
             return <NoWalletDetected/>;
         }
 
-        // The next thing we need to do, is to ask the user to connect their wallet.
-        // When the wallet gets connected, we are going to save the user's address
-        // in the component's state. So, if it hasn't been saved yet, we have
-        // to show the ConnectWallet component.
-        //
-        // Note that we pass it a callback that is going to be called when the user
-        // clicks a button. This callback just calls the _connectWallet method.
         if (!this.state.selectedAddress) {
             return (
                 <ConnectWallet
@@ -102,7 +94,7 @@ export class Dapp extends React.Component<{}, IState> {
             );
         }
 
-        // If the Dapp hasn't loaded yet
+        // If the Dapp hasn't loaded yet or executes transaction
         if (this.state.loading) {
             return <Loading/>;
         }
@@ -170,65 +162,52 @@ export class Dapp extends React.Component<{}, IState> {
         );
     }
 
+    /**
+     * Run when the user clicks the Connect
+     * connects the Dapp to the user's wallet, and initializes it
+     * Check if the user is on the correct network
+     * Reinitialize the Dapp whenever the user changes their account.
+     */
     async _connectWallet() {
-        // This method is run when the user clicks the Connect. It connects the
-        // dapp to the user's wallet, and initializes it.
-
-        // To connect to the user's wallet, we have to run this method.
-        // It returns a promise that will resolve to the user's address.
         const [selectedAddress] = await window.ethereum.request({method: 'eth_requestAccounts'});
 
-        // Once we have the address, we can initialize the application.
-
-        // First we check the network
         if (!this._checkNetwork()) {
             return;
         }
 
         this._initialize(selectedAddress);
 
-        // We reinitialize it whenever the user changes their account.
         window.ethereum.on("accountsChanged", ([newAddress]) => {
-            // `accountsChanged` event can be triggered with an undefined newAddress.
-            // This happens when the user removes the Dapp from the "Connected
-            // list of sites allowed access to your addresses" (Metamask > Settings > Connections)
-            // To avoid errors, we reset the dapp state
             if (newAddress === undefined) {
                 return this._resetState();
             }
-
             this._initialize(newAddress);
         });
 
-        // We reset the dapp state if the network is changed
         window.ethereum.on("chainChanged", ([networkId]) => {
             this._resetState();
         });
     }
 
+    /**
+     *  This method initialize the Dapp
+     *  1 - store the user's address in the component's state
+     *  2 - Initialize ethers
+     *  3 - Load the next question
+     * @param userAddress - The user's address
+     */
     _initialize(userAddress) {
-        // This method initializes the dapp
-
-        // We first store the user's address in the component's state
-        this.setState({
-            selectedAddress: userAddress,
-        });
-
-        // Then, we initialize ethers, fetch the token's data, and start polling
-        // for the user's balance.
-
-        // Fetching the token data and the user's balance are specific to this
-        // sample project, but you can reuse the same initialization pattern.
+        this.setState({selectedAddress: userAddress,});
         this._initializeEthers().then();
         this.loadQuestion().then();
     }
 
+    /**
+     *  1 - This method initialize ethers
+     *  2 - initialize the contract using that provider
+     */
     async _initializeEthers() {
-        // We first initialize ethers by creating a provider using window.ethereum
         this._provider = new ethers.providers.Web3Provider(window.ethereum);
-
-        // Then, we initialize the contract using that provider and the token's
-        // artifact. You can do this same thing with your contracts.
         this._quiz = new ethers.Contract(
             contractAddress.QuizFactory,
             QuizFactoryArtefact.abi,
@@ -238,27 +217,13 @@ export class Dapp extends React.Component<{}, IState> {
         console.log("network", network);
     }
 
-    // This method sends an ethereum transaction to transfer tokens.
-    // While this action is specific to this application, it illustrates how to
-    // send a transaction.
+    /**
+     * This method create a new quiz
+     * @param data - question and the hashed answer
+     */
     async addQuiz(data: QuizData) {
-        // Sending a transaction is a complex operation:
-        //   - The user can reject it
-        //   - It can fail before reaching the ethereum network (i.e. if the user
-        //     doesn't have ETH for paying for the tx's gas)
-        //   - It has to be mined, so it isn't immediately confirmed.
-        //     Note that some testing networks, like Hardhat Network, do mine
-        //     transactions immediately, but your dapp should be prepared for
-        //     other networks.
-        //   - It can fail once mined.
-        //
-        // This method handles all of those things, so keep reading to learn how to
-        // do it.
-
         try {
             // If a transaction fails, we save that error in the component's state.
-            // We only save one such error, so before sending a second transaction, we
-            // clear it.
             this._dismissTransactionError();
 
             // We send the transaction, and save its hash in the Dapp's state. This
@@ -272,29 +237,18 @@ export class Dapp extends React.Component<{}, IState> {
 
             // The receipt, contains a status flag, which is 0 to indicate an error.
             if (receipt.status === 0) {
-                // We can't know the exact error that made the transaction fail when it
-                // was mined, so we throw this generic one.
                 throw new Error("Transaction failed");
             }
             // Load the next question
             this.loadQuestion().then();
-
-            // If we got here, the transaction was successful, so you may want to
-            // update your state. Here, we update the user's balance.
         } catch (error) {
-            // We check the error code to see if this error was produced because the
-            // user rejected a tx. If that's the case, we do nothing.
+            // Do nothing if the user rejected the transaction
             if (error.code === ERROR_CODE_TX_REJECTED_BY_USER) {
                 return;
             }
-
-            // Other errors are logged and stored in the Dapp's state. This is used to
-            // show them to the user, and for debugging.
             console.error(error);
             this.setState({transactionError: error});
         } finally {
-            // If we leave the try/catch, we aren't sending a tx anymore, so we clear
-            // this part of the state.
             this.setState({txBeingSent: undefined});
         }
     }
@@ -403,16 +357,16 @@ export class Dapp extends React.Component<{}, IState> {
         }
     }
 
-    // This method checks if Metamask selected network is Localhost:8545
+    // This method checks if Metamask selected network is correct. Locally Localhost:8545
     _checkNetwork() {
-        if (window.ethereum.networkVersion === HARDHAT_NETWORK_ID) {
+        const network = window.ethereum.networkVersion;
+        if (network === HARDHAT_NETWORK_ID || network === POLYGON_NETWORK_ID) {
             return true;
         }
 
         this.setState({
-            networkError: 'Please connect Metamask to Localhost:8545'
+            networkError: 'Please select the matic mumbai testnet in your wallet'
         });
-
         return false;
     }
 }
