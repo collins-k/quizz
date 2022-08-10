19c099d27fc3a6d44ce06207bce60defd41a6255
collins-k <cyrille_08kamgaing@yahoo.fr>
2022-08-10 23:44:24 +0200

    feat(): refactor contract

--- contracts/QuizGame.sol
+++ contracts/QuizGame.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract QuizGame {
-    bytes32 public salt = bytes32("123123123");
+    bytes32 public constant SALT = bytes32("123123123");
     bytes32 public hashedAnswer;
     string public question;
     bool public solved;
@@ -11,15 +11,19 @@ contract QuizGame {
     event AnswerGuessed();
     event AnswerIncorrect();
 
+    error NotFounded(string msg);
+
     constructor(string memory _question, bytes32 _hashedAnswer) {
         question = _question;
         hashedAnswer = _hashedAnswer;
     }
 
     function guess(string calldata answer) public {
-        require(address(this).balance > 0, "You first have to fund the quiz");
+        if (address(this).balance <= 0) {
+            revert NotFounded("You first have to fund the quiz");
+        }
 
-        if (keccak256(abi.encodePacked(salt, answer)) == hashedAnswer) {
+        if (keccak256(abi.encodePacked(SALT, answer)) == hashedAnswer) {
             emit AnswerGuessed();
             (bool sent, bytes memory data) = payable(msg.sender).call{value : address(this).balance}("");
             require(sent, "Failed to send");
